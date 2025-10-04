dadfc5899c761aa68a24ebd9c121d13f1b326b7f
Gurnoor Singh Pannu <gurnxxrpannu@gmail.com>
2025-10-05 00:43:44 +0530

    android and ios module file

---
+++ shared/src/iosMain/kotlin/com/talkeys/shared/di/PlatformModule.ios.kt
@@ -0,0 +1,8 @@
+package com.talkeys.shared.di
+
+import org.koin.dsl.module
+
+actual val platformModule = module {
+    // iOS-specific dependencies will go here
+    // For example: database drivers, platform-specific implementations, etc.
+}
