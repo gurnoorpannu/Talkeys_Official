e0b9f55f29a61c68ca6ea4dd7f425c8b671853ae
Gurnoor Singh Pannu <gurnxxrpannu@gmail.com>
2025-10-14 22:01:56 +0530

    added firebase dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,6 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.android.kotlin.multiplatform.library) apply false
     alias(libs.plugins.android.lint) apply false
+    // Add the dependency for the Google services Gradle plugin
+    id("com.google.gms.google-services") version "4.4.4" apply false
 }
